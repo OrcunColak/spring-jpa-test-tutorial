3fc0d97acca11270d8937b721eb03e680156d99e
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-04 12:15:44 +0300

    @Sql example

--- src/test/java/com/colak/springjpatesttutorial/repository/PersonRepositoryH2Test.java
+++ src/test/java/com/colak/springjpatesttutorial/repository/PersonRepositoryH2Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
@@ -24,6 +25,20 @@ class PersonRepositoryH2Test {
         person.setFirstName("John");
         person.setLastName("Doe");
         personRepository.save(person);
+
+        // When
+        List<Person> foundPersons = personRepository.findByLastName("Doe");
+
+        // Then
+        assertThat(foundPersons).hasSize(1);
+        assertThat(foundPersons.get(0).getFirstName()).isEqualTo("John");
+    }
+
+    // Example to use @Sql annotation
+    @Test
+    @DisplayName("JUnit test for findByLastName operation")
+    @Sql(scripts = { "/populate-data.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    void whenFindByLastName_thenReturnPerson2() {
 
         // When
         List<Person> foundPersons = personRepository.findByLastName("Doe");
