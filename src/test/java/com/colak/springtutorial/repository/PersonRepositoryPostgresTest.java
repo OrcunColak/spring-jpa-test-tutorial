33a1542b9c139d92c03d7ed9e953c341421f41da
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-18 11:23:47 +0300

    new test for find

--- src/test/java/com/colak/springtutorial/repository/PersonRepositoryPostgresTest.java
+++ src/test/java/com/colak/springtutorial/repository/PersonRepositoryPostgresTest.java
@@ -7,20 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 // Disable the transactional and roll back in @DataJpaTest
-@Transactional(propagation = Propagation.NOT_SUPPORTED)
+// @Transactional(propagation = Propagation.NOT_SUPPORTED)
 // Disable H2
 // See https://erkanyasun.medium.com/a-deep-dive-into-the-latest-spring-boot-3-4-0-m3-52ca7e03db2d
 // With 3.4.0-M3 the @AutoConfigureTestDatabase annotation has been upgraded to automatically detect if a database is sourced from a container.
@@ -33,7 +32,7 @@ class PersonRepositoryPostgresTest {
     @SuppressWarnings("resource")
     @Container
     @ServiceConnection
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
+    private static final PostgreSQLContainer<?> CONTAINER =
             new PostgreSQLContainer<>(DockerImageName.parse("postgres"))
                     .withPassword("inmemory")
                     .withUsername("inmemory");
@@ -43,7 +42,7 @@ class PersonRepositoryPostgresTest {
 
     @Test
     @DisplayName("JUnit test for findByLastName operation")
-    void whenFindByLastName_thenReturnPerson() {
+    void findByLastName() {
         // Given
         Person person = new Person();
         person.setFirstName("John");
@@ -54,8 +53,31 @@ class PersonRepositoryPostgresTest {
         List<Person> foundPersons = personRepository.findByLastName("Doe");
 
         // Then
-        assertThat(foundPersons).hasSize(1);
-        assertThat(foundPersons.get(0).getFirstName()).isEqualTo("John");
+        assertThat(foundPersons).hasSizeGreaterThanOrEqualTo(1);
+
+        Person firstPerson = foundPersons.getFirst();
+        assertThat(firstPerson.getFirstName()).isEqualTo("John");
+    }
+
+    @Test
+    @DisplayName("JUnit test for findById operation")
+    void findById() {
+        // Given
+        Person person = new Person();
+        person.setFirstName("Jane");
+        person.setLastName("Doe");
+        Person savedPerson = personRepository.save(person);
+
+        // When
+        Optional<Person> foundPersonOptional = personRepository.findById(savedPerson.getId());
+
+        // Then
+        assertThat(foundPersonOptional).isPresent()
+                .hasValueSatisfying(foundPerson -> {
+                            assertThat(foundPerson.getFirstName()).isEqualTo("Jane");
+                            assertThat(foundPerson.getLastName()).isEqualTo("Doe");
+                        }
+                );
     }
 }
 
